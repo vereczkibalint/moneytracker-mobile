bf0f4eba6ed68e241ffd07381e8f933b4bd51e48
vbalint <balint.vereczki@adnovum.hu>
2022-01-29 19:34:59 +0100

    tabs: icons are changing if the tab is selected

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage implements OnInit {
+  _selectedTab: string;
+  _TABS = {
+    TAB_HOME: 'home',
+    TAB_BUDGETS: 'budgets',
+    TAB_STATISTICS: 'statistics',
+    TAB_SETTINGS: 'settings'
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  _setSelectedTab(tab) {
+    this._selectedTab = tab.tab;
+  }
+
+  _checkIfTabIsSelected(tab) {
+    return this._selectedTab === tab;
+  }
 }
