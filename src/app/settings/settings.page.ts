eb314cdceb2ad411517735e103e97380ce53c815
vereczkibalint <vereczkibalint@gmail.com>
2022-04-11 14:47:20 +0200

    auth: logout implementation

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {IonSelect} from "@ionic/angular";
+import {AuthService} from "../auth/auth.service";
 
 @Component({
   selector: 'app-settings',
@@ -10,7 +11,7 @@ export class SettingsPage implements OnInit {
   currentTheme: string;
   @ViewChild('appearanceSelector') appearanceSelector: IonSelect;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
     this.currentTheme = 'dark';
@@ -18,5 +19,9 @@ export class SettingsPage implements OnInit {
 
   async _toggleAppearanceSelector() {
     await this.appearanceSelector.open();
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
