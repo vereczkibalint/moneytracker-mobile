2be3cd934ab8d2852b991a309cc816f5a5f272c3
vbalint <vereczkibalint@gmail.com>
2022-04-24 19:32:09 +0200

    auth: added error handling of expired JWT token or unauthorized call to backend

--- src/app/core/interceptors/request.interceptor.ts
+++ src/app/core/interceptors/request.interceptor.ts
@@ -10,17 +10,20 @@ import { map, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { Storage } from "@ionic/storage";
 import {StorageService} from "../services/storage.service";
+import {Router} from "@angular/router";
+import {AuthService} from "../../auth/auth.service";
 
 @Injectable()
 export class HttpConfigInterceptor implements HttpInterceptor {
-  constructor(private storageService: StorageService) {}
+  TOKEN_KEY = 'jwt-token';
+  constructor(private storage: Storage, private authService: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return from(this.handleIntercept(req, next));
   }
 
   async handleIntercept(req: HttpRequest<any>, next: HttpHandler) {
-    const token = await this.storageService.getKey('jwt-token');
+    const token = await this.storage.get(this.TOKEN_KEY);
 
     if(token) {
       req = req.clone({
@@ -36,6 +39,11 @@ export class HttpConfigInterceptor implements HttpInterceptor {
       }),
       catchError((error: HttpErrorResponse) => {
         console.error(error);
+        if(error.status === 401 || error.status === 403) {
+          this.storage.remove(this.TOKEN_KEY).then(() => {
+            this.authService.logout();
+          });
+        }
         return throwError(error);
       })
     ).toPromise();
